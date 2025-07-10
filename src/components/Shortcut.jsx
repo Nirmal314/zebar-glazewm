950dffca68ca57bcf9919f7f6fafc8f5e986497d
Nirmal Ambasana <nambasana033@gmail.com>
2025-07-10 17:52:50 +0530

    My version

--- src/components/Shortcut.jsx
+++ src/components/Shortcut.jsx
@@ -1,22 +1,22 @@
 const Shortcut = ({ commandRunner, commands, iconClass, name }) => {
-    const style = {
-        cursor: 'pointer',
-        fontSize: '10px',
-        marginLeft: '5px',
-    }
+  const style = {
+    cursor: "pointer",
+    fontSize: "14px",
+    marginLeft: "5px",
+  };
 
-    const onClick = () => {
-        for (const command of commands) {
-            commandRunner(command);
-        }
+  const onClick = () => {
+    for (const command of commands) {
+      commandRunner(command);
     }
+  };
 
-    return (
-        <button className="shortcut clean-button" style={style} onClick={() => onClick()}>
-            <i className={`nf ${iconClass}`}></i>
-            <span>{name}</span>
-        </button>
-    );
+  return (
+    <button className="box" style={style} onClick={() => onClick()}>
+      <i className={`nf ${iconClass}`}></i>
+      <span>{name}</span>
+    </button>
+  );
 };
 
 export default Shortcut;
